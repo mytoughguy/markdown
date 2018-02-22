e83b1a29cce0028ccfaf309ed9e131084d61a7d3
mytoughtuy <648647773@qq.com>
2018-02-22 15:45:03 +0800

    markdown最基本原理

--- markdown.js
+++ markdown.js
@@ -3,8 +3,10 @@
     Markdown 是一种方便记忆、书写的纯文本标记语言，用户可以使用这些标记符号以最小的输入代价生成极富表现力的文档
     Cmd Markdown 是这个星球上我们已知的、最好的 Markdown 工具
     - 使用cmd markdown编辑器作为我的主要编辑器
-    
+
 -找教程
+    简书教程:
+        - 要点是 通过 https://github.com/chjj/marked/blob/master/lib/marked.js 将markdown语法转换为html页面
 
 -学习教程
 */
